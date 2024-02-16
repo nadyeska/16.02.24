bafd92d1873e7da87f39b1c81e514e3cffa5f5fa
nadyeskaquint <142436834+nadyeska@users.noreply.github.com>
2024-02-16 12:20:41 -0300

    Add files via upload

--- foreach.js
+++ foreach.js
@@ -1,5 +1,5 @@
 var tempoFilme = 60; // em segundos
-var  inicioFilme = 60; // em segundos
+var  inicioFilme = 0; // em segundos
 
 for (var t = 0; t < tempoFilme; t++) {
     setTimeout(function() {
